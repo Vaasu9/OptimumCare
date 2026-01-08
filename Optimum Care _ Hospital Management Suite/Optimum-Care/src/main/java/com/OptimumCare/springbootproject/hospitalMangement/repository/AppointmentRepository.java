fef1e79522a7de82b9b1bb9f37fd5e9a31d29a63
Vaasu Sriavstava <149596875+Vaasu9@users.noreply.github.com>
2026-01-08 15:49:54 +0530

    Delete Optimum Care _ Hospital Management Suite directory

--- Optimum Care _ Hospital Management Suite/Optimum-Care/src/main/java/com/OptimumCare/springbootproject/hospitalMangement/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.OptimumCare.springbootproject.hospitalMangement.repository;
-
-import com.OptimumCare.springbootproject.hospitalMangement.entity.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
